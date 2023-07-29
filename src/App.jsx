0ce0841d4a28ac69f3edee79b9ea9c027ff5203a
Vishu <Vishwajeetsinghchauhan63@gmail.com>
2023-07-29 13:44:49 +0530

    Navbar setup done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import Navbar from "./components" ;
+import Navbar from "./components/navbar/Navbar" ;
+import "./App.css"
 
 export default function App() {
 
@@ -9,8 +10,8 @@ export default function App() {
 
     <div className ="App">
      <div className="gradient_bg">
-      <h1>Don't forget that I am here for you. I love you. </h1>
-       // Navbar here
+      
+      <Navbar />
 
       </div> 
     </div>
