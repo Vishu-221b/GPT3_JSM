ce75ce1597775dd75891bae4e9047f2dcb12cd89
Vishu <Vishwajeetsinghchauhan63@gmail.com>
2023-07-29 10:26:22 +0530

    Files setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Navbar from "./components" ;
+
+export default function App() {
+
+
+  return (
+
+    <div className ="App">
+     <div className="gradient_bg">
+      <h1>Don't forget that I am here for you. I love you. </h1>
+       // Navbar here
+
+      </div> 
+    </div>
+  );
+}
