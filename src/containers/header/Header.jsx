17c205f35ebcdbf85378ddbffd29ec3fda97f750
Vishu <Vishwajeetsinghchauhan63@gmail.com>
2023-07-30 13:07:32 +0530

    Header css also completed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,24 +6,26 @@ import "./header.css"
 export default function Header()
     {
         return(
-           <div>
-                <div>
-                    <h1>Let’s Build Something
-amazing with GPT-3
-OpenAI</h1>
+           <div className="gpt3__header section__padding" id="home">
+                <div className="gpt3__header-content">
+
+                    <h1 className="gradient__text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
                     <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-                    <div>
+
+                    <div className="gpt3__header-content__input">
                         <input type="email" placeholder="Enter your email"/>
                         <button type="button">Get Started </button>
                     </div>
-                    <div>
+
+                    <div className="gpt3__header-content__people">
                         <img src={people} />
                         <p>1,600 people requested access a visit in last 24 hours</p>
                     </div>
-                </div>
-                <div>
-                    <img src ={ai} />
-                </div>
+
+                </div >
+                       <div className="gpt3__header-image">
+                           <img src ={ai} />
+                       </div>
             </div>
         )
     }
